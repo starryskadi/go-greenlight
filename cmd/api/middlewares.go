9782b4738ffbbcf2b3d9fc7a01116bdede1c971a
Starry Skadi <starryskadi@gmail.com>
2025-05-19 00:48:40 +0700

    feat: add vendor function

--- cmd/api/middlewares.go
+++ cmd/api/middlewares.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -12,6 +11,7 @@ import (
 	"time"
 
 	"github.com/felixge/httpsnoop"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 	"kyawzayarwin.com/greenlight/internal/data"
 	"kyawzayarwin.com/greenlight/internal/validator"
@@ -61,11 +61,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
